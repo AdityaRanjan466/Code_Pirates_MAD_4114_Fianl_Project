347e5624406e33809d3b7983b1a0c7a07e10f327
AdityaRanjan466 <C0880466@mylambton.ca>
2023-01-24 14:39:51 -0500

    iOS Notes app created

--- notesApp/Controller/AppDelegate.swift
+++ notesApp/Controller/AppDelegate.swift
@@ -1,9 +1,3 @@
-//
-//  AppDelegate.swift
-//  notesApp
-//
-//  Created by apple on 19/01/23.
-//
 
 import UIKit
 import CoreData
@@ -41,7 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "notesApp")
+        let container = NSPersistentContainer(name: "Notes")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
